62eeacdb1945dba5cdaa75494d85ea2a1af33942
x0r5 <obivan05@gmail.com>
2019-08-14 16:05:09 +0200

    database access

--- src/main/java/hu/sebestyenphoto/spring101/HotelBooking.java
+++ src/main/java/hu/sebestyenphoto/spring101/HotelBooking.java
@@ -1,6 +1,17 @@
 package hu.sebestyenphoto.spring101;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class HotelBooking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
     private String hotelName;
     private double pricePerNight;
     private int nbOfNights;
@@ -22,5 +33,13 @@ public class HotelBooking {
 
     public int getNbOfNights() {
         return nbOfNights;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
